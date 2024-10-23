c22d48ba286e491d217792a9e234fa45f0a0943a
pyankie <zpyankie@gmail.com>
2024-10-23 13:20:17 +0300

    Build a menu-bar for mobiles.

---
+++ bs-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  server: true,
+  files: ["*.html", "*.css", "*.js"],
+  notify: false, // Disable the "BrowserSync connected" notification
+};
--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const input = document.querySelector(".search-input-wrapper .input");
 const inputWrapper = document.querySelector(".search-input-wrapper");
 
-// const collapsibles = document.querySelectorAll(".collapsible");
-// collapsibles.forEach((item) =>
-//   item.addEventListener("click", function () {
-//     this.classList.toggle("collapsible--expanded");
-//   })
-// );
-
+const collapsibles = document.querySelector(".menu-bar__content");
 const searchBlock = document.querySelector(".search-block");
 
 input.addEventListener("click", () => {
   searchBlock.classList.toggle("collapsible--expanded");
   inputWrapper.classList.toggle("outline");
+});
+
+const menuBarFeatureToggler = document.querySelector(".toggler");
+menuBarFeatureToggler.addEventListener("click", () => {
+  collapsibles.classList.toggle("collapsible--expanded");
+});
+
+const menuBarToggler = document.querySelector(".nav-menu__close-icon");
+const navMenu = document.querySelector(".nav-menu");
+menuBarToggler.addEventListener("click", () => {
+  navMenu.classList.add(".collapse");
 });
